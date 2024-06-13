bba7f3dd3211d8dae98a3126361f0154e8cb1292
Amit Kumar <kumaramitrr123@gmail.com>
2024-06-14 04:33:42 +0530

    Added header file include signin , about , home , and Search option

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import SignIn from './pages/Signin'
 import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 
 
@@ -12,6 +13,7 @@ import Profile from './pages/Profile'
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path = '/' element = {<Home/>}/>
         <Route path = '/Sign-In' element = {<SignIn/>}/>
