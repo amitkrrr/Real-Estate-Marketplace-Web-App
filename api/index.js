0005451dc9e2e7c0d80c970edb2765d549cd7f5e
Amit Kumar <kumaramitrr123@gmail.com>
2024-06-14 23:19:46 +0530

    created user model and test api

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import userRouter from './routes/user.routes.js'
 dotenv.config();
 
 mongoose.connect(process.env.mongo).then (() => {
@@ -18,4 +18,6 @@ const app = express(); // it gives lot of method to use
 app.listen(3000 , () => {
     console.log('server is running on port 3000!!!!');
     }
-)
+);
+
+app.use("/api/user" , userRouter);
