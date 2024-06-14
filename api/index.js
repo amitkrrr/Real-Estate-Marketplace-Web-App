e467a735717141412c43dde9bc5e4b84bc639c05
Amit Kumar <kumaramitrr123@gmail.com>
2024-06-14 07:20:04 +0530

    create and run server

---
+++ api/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express(); // it gives lot of method to use
+
+app.listen(3000 , () => {
+    console.log('server is running on port 3000!!!!');
+    }
+);
