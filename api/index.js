49dfe0e4c72de79d7e80c6a318dc28b9c40cc373
Amit Kumar <kumaramitrr123@gmail.com>
2024-06-14 07:59:17 +0530

    mongodb connected

--- api/index.js
+++ api/index.js
@@ -1,8 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.mongo).then (() => {
+    console.log('connected to db');
+}).catch((err) => {
+    console.log(err);
+});
+
+
+// mongoose.connect("mongodb+srv://amit:Amit@063@realestate.p0qiasx.mongodb.net/?retryWrites=true&w=majority&appName=realEstate")
 
 const app = express(); // it gives lot of method to use
 
 app.listen(3000 , () => {
     console.log('server is running on port 3000!!!!');
     }
-);
+)
