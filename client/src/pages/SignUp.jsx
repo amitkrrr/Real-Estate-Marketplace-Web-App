07d2c38f32b4da292dbfd8a0eeaf67a20dc656da
Amit Kumar <kumaramitrr123@gmail.com>
2024-06-17 07:42:15 +0530

    create signin api

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -43,9 +43,9 @@ export default function SignUp() {
 
   console.log(formData);
   return (
-    <div className='p-3 max-w-large mx-auto'>
+    <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'> Sign Up </h1>
-      <form onSubmit = {handleSubmit} className='flex flex-col gap-4 '>
+      <form onSubmit = {handleSubmit} className='flex flex-col gap-4'>
         <input type = "text" placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
         <input type = "email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
         <input type = "password" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
