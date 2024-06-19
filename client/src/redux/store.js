581ac8a7c1692b16c13c09cfa56b8815bfbf9993
Amit Kumar <kumaramitrr123@gmail.com>
2024-06-19 14:52:47 +0530

    add persist Reducer to store signin data in local storage

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice.js';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import persistStore from 'redux-persist/es/persistStore';
 
+const rootReducer = combineReducers({user: userReducer});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1
+};
+
+const persistedReducer = persistReducer(persistConfig , rootReducer);
+
+// persisted reducer store previous user data globally after refresh , without it after refreshing user goes signout and again required to signin
 
 export const store = configureStore({
-  reducer: {user: userReducer},
+  // reducer: {user: userReducer},
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => 
     getDefaultMiddleware({
         serializableCheck: false,
     }),
 
-})
+});
+
+export const persistor = persistStore(store);
