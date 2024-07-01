0fa96440c1b18fcc79d0d7d1fde6ac1a3f46bbd3
Amit Kumar <kumaramitrr123@gmail.com>
2024-07-01 12:54:15 +0530

    add on change and onSubmit functionality in search page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
 import UpdateListing from './pages/UpdateListing'
 import Listing from './pages/Listing'
+import Search from './pages/Search'
 
 
 
@@ -25,6 +26,7 @@ export default function App() {
         <Route path = '/Sign-In' element = {<SignIn/>}/>
         <Route path = '/Sign-Up' element = {<SignUp/>}/>
         <Route path = '/about' element = {<About/>}/>
+        <Route path='/search' element={<Search />} />
         <Route element = {<PrivateRoute/>} >
           <Route path = '/profile' element = {<Profile/>}/>
           <Route path = '/create-listing' element = {<CreateListing/>}/>
