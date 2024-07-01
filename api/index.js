43a14e3bc5dfea6658b819138d1696366ee6283c
Amit Kumar <kumaramitrr123@gmail.com>
2024-07-01 13:42:22 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.mongo).then (() => {
@@ -13,7 +14,7 @@ mongoose.connect(process.env.mongo).then (() => {
     console.log(err);
 });
 
-
+const __dirname = path.resolve();
 // mongoose.connect("mongodb+srv://amit:Amit@063@realestate.p0qiasx.mongodb.net/?retryWrites=true&w=majority&appName=realEstate")
 
 const app = express(); // it gives lot of method to use
@@ -28,6 +29,12 @@ app.listen(3000 , () => {
 app.use("/api/user" , userRouter);
 app.use("/api/auth" , authRouter);
 app.use("/api/listing" , listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err , req , res , next) => {
     const statusCode = err.statusCode || 500;
