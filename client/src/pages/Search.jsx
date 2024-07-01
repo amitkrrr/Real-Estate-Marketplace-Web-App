d71118d20d39c53e4cd9f7bd4aad5b39d82ec7c2
Amit Kumar <kumaramitrr123@gmail.com>
2024-07-01 13:19:06 +0530

    add show more listings functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -17,6 +17,7 @@ export default function Search() {
 
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
+   const [showMore, setShowMore] = useState(false);
   
 
 
@@ -56,7 +57,11 @@ export default function Search() {
       const searchQuery = urlParams.toString();
       const res = await fetch(`/api/listing/get?${searchQuery}`);
       const data = await res.json();
-     
+     if (data.length > 8) {
+        setShowMore(true);
+      } else {
+        setShowMore(false);
+      }
       setListings(data);
       setLoading(false);
     };
@@ -112,6 +117,20 @@ export default function Search() {
     navigate(`/search?${searchQuery}`);
   };
 
+
+   const onShowMoreClick = async () => {
+    const numberOfListings = listings.length;
+    const startIndex = numberOfListings;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('startIndex', startIndex);
+    const searchQuery = urlParams.toString();
+    const res = await fetch(`/api/listing/get?${searchQuery}`);
+    const data = await res.json();
+    if (data.length < 9) {
+      setShowMore(false);
+    }
+    setListings([...listings, ...data]);
+  };
 
   return (
     <div className='flex flex-col md:flex-row'>
@@ -236,12 +255,14 @@ export default function Search() {
             ))}
 
           
+             {showMore && (
             <button
-            
+              onClick={onShowMoreClick}
               className='text-green-700 hover:underline p-7 text-center w-full'
             >
               Show more
             </button>
+          )}
           
         </div>
       </div>
