2668909cc5ef60971ffd361ed941ba1de0edde8f
Amit Kumar <kumaramitrr123@gmail.com>
2024-07-01 09:12:25 +0530

    completed listing functionality and contact landlord

--- api/controllers/lisitng.controller.js
+++ api/controllers/lisitng.controller.js
@@ -1,14 +1,13 @@
-import Listing from "../models/listing.model.js";
-import errorHandler from "../utils/error.js";
+import Listing from '../models/listing.model.js';
+import  errorHandler  from '../utils/error.js';
 
-export const createListing = async (req , res , next) => {
-    try{
-        const listing = await Listing.create(req.body);
-        return res.status(201).json(listing);
-    }
-    catch(error){
-        next(error);
-    }
+export const createListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.create(req.body);
+    return res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const deleteListing = async (req, res, next) => {
@@ -51,15 +50,15 @@ export const updateListing = async (req, res, next) => {
   }
 };
 
-export const getListing = async(req , res , next) =>{
-  try{
+export const getListing = async (req, res, next) => {
+  try {
     const listing = await Listing.findById(req.params.id);
-    if(!listing){
-      return next(errorHandler(404 , 'Listing not found!'));
+    if (!listing) {
+      return next(errorHandler(404, 'Listing not found!'));
     }
     res.status(200).json(listing);
-  }
-  catch(error){
+  } catch (error) {
     next(error);
   }
 };
+
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -8,5 +8,6 @@ router.post('/create' ,verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id', verifyToken, updateListing);
 router.get('/get/:id' , getListing);
+
 export default router;
 
